c4413d42127078e05512646d3afc1d4a78a9942d
Ricardo Firme de Oliveira <rick.firmeoliveira2@gmail.com>
2024-08-12 13:43:35 -0300

    feat: alterar visualizacao botao de adicionar

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,12 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Tarefa } from '../../Models/Tarefa';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ButtonComponent],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css',
 })
@@ -15,6 +16,7 @@ export class AddTaskComponent {
   tarefa: string = '';
   categoria: string = '';
   concluido: boolean = false;
+  mostrarAddTarefa: boolean = false;
 
   onSubmit() {
     if (!this.tarefa) {
@@ -32,5 +34,9 @@ export class AddTaskComponent {
     this.tarefa = '';
     this.categoria = '';
     this.concluido = false;
+  }
+
+  alteraVisualizacao(valor: boolean) {
+    this.mostrarAddTarefa = valor;
   }
 }
